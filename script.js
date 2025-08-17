4b31704b708c87fbe4ee17808d6a87f03567211f
MohdFazil45 <mdfazil846@gmail.com>
2025-08-17 18:59:58 +0530

    page 1,3,4 done

--- script.js
+++ script.js
@@ -1,41 +1,65 @@
-let tl = gsap.timeline()
-tl.from(".line h1", {
-    y: 150,
-    stagger: 0.25,
-    duration: 0.6,
-    delay: 0.5
-})
-tl.from("#line1-part1", {
-    opacity: 0,
-    onStart: function () {
-        const loaderTimer = document.querySelector("#line1-part1 h5")
-        let grow = 0
-        setInterval(() => {
-            if (grow < 100) {
-                loaderTimer.innerHTML = grow++
-            } else {
-                loaderTimer.innerHTML = grow
-            }
-        }, 30);
-    }
-})
+function loadingAnimation() {
+    let tl = gsap.timeline()
+    tl.from(".line h1", {
+        y: 150,
+        stagger: 0.25,
+        duration: 0.5,
+        delay: 0.5
+    })
+    tl.from("#line1-part1", {
+        opacity: 0,
+        onStart: function () {
+            const loaderTimer = document.querySelector("#line1-part1 h5")
+            let grow = 0
+            setInterval(() => {
+                if (grow < 100) {
+                    loaderTimer.innerHTML = grow++
+                } else {
+                    loaderTimer.innerHTML = grow
+                }
+            }, 33);
+        }
+    })
 
-tl.to(".line h2",{
-    animationName:"anime",
-    opacity:1
-})
-tl.to("#loader", {
-    opacity: 0,
-    duration: 0.3,
-    delay: 3
-})
-tl.from("#page1",{
-    delay:0.2,
-    y:1200,
-    opacity:0,
-    duration:0.5,
-    ease:Power4
-})
-tl.to("#loader",{
-    display:"none"
-})
+    tl.to(".line h2", {
+        animationName: "anime",
+        opacity: 1
+    })
+    tl.to("#loader", {
+        opacity: 0,
+        duration: 0.3,
+        delay: 3
+    })
+    tl.from("#page1", {
+        delay: 0.2,
+        y: 1200,
+        opacity: 0,
+        duration: 0.5,
+        ease: Power4
+    })
+    tl.to("#loader", {
+        display: "none"
+    })
+    tl.from("nav",{
+        opacity:0
+    })
+    tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero3 h3,#hero4 h1",{
+        y:120,
+        stagger:0.2
+    })
+    tl.from("#video-container",{
+        y:120,
+        stagger:0.2
+    })
+}
+function cursorAnimation(){
+    document.addEventListener("mousemove",(dets)=>{
+    gsap.to("#crsr",{
+        left:dets.x,
+        top:dets.y
+    })
+    })
+    Shery.makeMagnet("#nav-part2 h4");
+}
+loadingAnimation()
+cursorAnimation()
